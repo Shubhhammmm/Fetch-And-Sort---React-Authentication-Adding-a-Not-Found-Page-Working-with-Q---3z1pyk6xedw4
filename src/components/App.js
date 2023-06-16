9892918376d5e71e4b4058e89953b7b9c1ffd6c6
Shubham Shrivastava <128039947+Shubhhammmm@users.noreply.github.com>
2023-06-16 09:58:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,29 +5,55 @@ const App = () => {
 
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [sortAscending, setSortAscending] = useState(true);  
+  const [sortAscending, setSortAscending] = useState(true); 
+  const fetchData = async()=>{
+    setIsLoading(true)
+    try{
+      const res = await fetch('https://content.newtonschool.co/v1/pr/main/users');
+      const data = await res.json()
+      setUsers(data);
+      setIsLoading(false);
+    }
+    catch(e){
+      setIsLoading(false);
+    }
+      
+  }
+  const handleSort = async ()=>{
+    const s_arr = [...users].sort((a,b)=>{
+      const nameA = a.name.length;
+      const nameB = b.name.length;
+      return (sortAscending? nameA-nameB :nameB-nameA)
+    })
+    setUsers(s_arr);
+    setSortAscending(!sortAscending);
+  }
 
   return (
     <div id="main">
       <h2>User List</h2>
-      <button className="fetch-data-btn">Fetch User Data</button>
-      <button className="sort-btn">
-        "Sort by name length (ascending)"
-        "Sort by name length (descending)"
+      <button className="fetch-data-btn" onClick={fetchData}>Fetch User Data</button>
+      <button className="sort-btn" onClick={handleSort}>
+       {sortAscending ? "Sort by name length (ascending)" :"Sort by name length (descending)" }
       </button>
-      <p>Loading...</p>
-      <div className='users-section'>
-          <li>
-            <section className='id-section'></section>
+  {isLoading?  <p>Loading...</p>:(<div className='users-section'>
+    {users.map(e=>(
+      <li key = {e.id}>
+            <section className='id-section'>{e.id}</section>
             <section className='name-email-section'>
-              <p className='name'>Name: </p>
-              <p className='email'>Email: </p>
+              <p className='name'>Name:{e.name} </p>
+              <p className='email'>Email:{e.email} </p>
             </section>
           </li>
-      </div>
+         ))}
+         
+      </div>)}
+
+     
+      
     </div>
   )
-}
+    }
 
 
 export default App;
